9b70c23365219d7a88c48149e75a5c421ec4919c
James De Sena <jamesdesena27@gmail.com>
2024-07-18 17:29:52 +0800

    xzcv

--- backend/controllers/BlogController.js
+++ backend/controllers/BlogController.js
@@ -5,7 +5,7 @@ const requireAuth = require('../utils/authToken');
 
 const GetAllBlog = async (req, res) => {
   try {
-    const result = await BlogModel.find({}).sort({ dateCreated: -1 }).allowDiskUse(true);
+    const result = await BlogModel.find({});
     res.status(200).json(result);
   } catch (err) {
     res.status(500).json({ message: err.message });
